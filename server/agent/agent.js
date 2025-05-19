f5a2ee203bf000783421c95b0364adf523649fba
BernalHQ <bernalhq19@gmail.com>
2025-05-19 10:44:09 -0600

    AI agent rest api

--- server/agent/agent.js
+++ server/agent/agent.js
@@ -24,12 +24,13 @@ const llm = new ChatAnthropic({
 
 const checkpointSaver = new MemorySaver();
 
-const agent = createReactAgent({
+export const agent = createReactAgent({
  llm: llm,
  tools: [weatherTool],	
  checkpointSaver: checkpointSaver
 });
 
+/*
 const response = await agent.invoke({
     messages: [{
         role: 'user',
@@ -55,42 +56,4 @@ const followup = await agent.invoke({
 
 console.log(response.messages.at(-1).content); 
 console.info('other: ', followup.messages.at(-1).content); 
-
-//import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
-/*
-import { ChatOpenAI } from "@langchain/openai";
-import { MemorySaver } from "@langchain/langgraph";
-import { HumanMessage } from "@langchain/core/messages";
-import { createReactAgent } from "@langchain/langgraph/prebuilt";
-
-// Define the tools for the agent to use
-//const agentTools = [new TavilySearchResults({ maxResults: 3 })];
-const agentModel = new ChatOpenAI({ temperature: 0 });
-
-// Initialize memory to persist state between graph runs
-const agentCheckpointer = new MemorySaver();
-const agent = createReactAgent({
-  llm: agentModel,
-  tools: []
-});
-
-// Now it's time to use!
-const agentFinalState = await agent.invoke(
-  { messages: [new HumanMessage("what is the current weather in sf")] },
-  { configurable: { thread_id: "42" } },
-);
-
-console.log(
-  agentFinalState.messages[agentFinalState.messages.length - 1].content,
-);
-
-const agentNextState = await agent.invoke(
-  { messages: [new HumanMessage("what about ny")] },
-  { configurable: { thread_id: "42" } },
-);
-
-console.log(
-  agentNextState.messages[agentNextState.messages.length - 1].content,
-);
-
 */
